6fd13ddae7688d68ea07f0e6b52c3857b197fb6c
al-dnz <paleozene@gmail.com>
2022-02-09 16:37:14 +0100

    updt

--- CPP00/ex01/Phonebook.hpp
+++ CPP00/ex01/Phonebook.hpp
@@ -5,7 +5,7 @@ class Phonebook
 	public:
 		Contact			contact_tab[8];
 		int				contact_number;
-		Phonebook() :	contact_number(0) {};
+		Phonebook():	contact_number(0) {};
 		void			print(void);
 	
 	private:
